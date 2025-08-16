73e6842125d75eaabb255b6d84ee74b30f74c416
nileshMaurya123 <nilesh7874@gmail.com>
2025-08-16 21:00:10 +0530

    6 questions solved

--- arrayquestions.js
+++ arrayquestions.js
@@ -1,3 +1,80 @@
-// 1. What is an array in JavaScript?
+// Q1. What is an array in JavaScript?
 
-// Answer
+// Answer 1 :
+
+/*
+
+ In JavaScript, an array is a special type of object used to store multiple values in a single variable. 
+ It can hold different data types (numbers, strings, objects, even other arrays).
+
+Example :  let a = [1, 2, 3, 4, 5, 6, 7];
+
+ */
+
+// Q2. How do you create an array in Javascript?
+
+// Answer 2.
+
+/*
+
+In Javascript you can create an array using square brackets []
+
+Example: let fruits = ['Apple', 'Banana', 'Mango', 'Orange']
+*/
+
+// Q3. How do you access the first element of an array?
+
+// Answer 3.
+
+/*
+
+To access the first element of an array in JavaScript, use index 0
+
+Example : let b = [1,2,3,4,5,6,7,8];
+let firstindex = b[0];
+console.log(firstindex);
+*/
+
+// Q4. How do you access the last element of an array?
+
+// Answer 4.
+
+/*
+To access the last element of an array in JavaScript, you can use the array's length property
+
+Method : Using array.length - 1
+
+let fruits = ["apple", "banana", "cherry"];
+
+let lastFruit = fruits[fruits.length - 1];
+
+*/
+
+// Q5. How do you find the length of an array?
+
+// Answer 5.
+
+/*
+By using .length property we are able to find length of an array
+
+
+let cars = ["BMW", "Mercedes", "Toyota"];
+let length = cars.length;
+
+*/
+
+// Q6. How do you add an element to the end of an array?
+
+// Answer 6.
+
+/*
+
+By using .push() method we can add element to the end of an array.
+*/
+
+/*
+
+example :  let cars = ["BMW", "Mercedes", "Toyota"];
+cars.push('Herculeus')
+console.log(cars);
+*/
